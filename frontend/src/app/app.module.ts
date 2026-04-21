91173d34da5068eb545a95f3b8499b47ab78ae8d
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:30:24 +0200

    Fix app.module.ts: use SmtpMailPluginModule and smtpmailPluginSpecification

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -75,7 +75,7 @@ import {TeamsModule} from "@valtimo/teams";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/smtpmail";
+import {SmtpMailPluginModule, smtpmailPluginSpecification,} from "@valtimo-plugins/smtpmail";
 
 export function tabsFactory() {
   return new Map<string, object>([
@@ -124,7 +124,7 @@ export function tabsFactory() {
     ProcessModule,
     ReactiveFormsModule,
     ResourceModule,
-    SamplePluginModule,
+    SmtpMailPluginModule,
     SecurityModule,
     SseModule,
     SwaggerModule,
@@ -145,7 +145,7 @@ export function tabsFactory() {
     {
       provide: PLUGINS_TOKEN,
       useValue: [
-        samplePluginSpecification,
+        smtpmailPluginSpecification,
       ],
     },
   ],
